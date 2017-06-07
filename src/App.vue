dcd3842bce1e916ac18a2e6ee8f1b61539cbc716
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-06-07 16:59:44 +0100

    test frontend setup with basic message template

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <message>Hello There</message>
   </div>
 </template>
 
 <script>
+import Message from './components/Message';
+
 export default {
   name: 'app',
+
+  components: {
+    Message,
+  },
+
+  data() {
+    return {
+
+    };
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .box {
+    background: grey;
+    padding: 10px;
+    border: 1px solid grey;
+  }
+</style>
